98de86c957c074890f5d818a9a36759d1f21c30f
Kasper Tvede <kasper@webmasteren.eu>
2017-12-11 07:51:21 +0100

    added more assert statements.

---
+++ csenseTests/extensions/CollectionsExtensions.swift
@@ -0,0 +1,22 @@
+//
+// Created by Kasper T on 09/12/2017.
+// Copyright (c) 2017 commonsense. All rights reserved.
+//
+
+import Foundation
+
+public extension Optional where Wrapped: Collection, Wrapped.IndexDistance == Int {
+
+    public func assertCount(_ size: Int, message: String = "",
+                            file: StaticString = #file, line: UInt = #line) {
+        self.assertNotNil()
+        self.assertCount(size, message: message, file: file, line: line)
+    }
+}
+
+public extension Collection where IndexDistance == Int {
+    public func assertCount(_ size: Int, message: String = "",
+                            file: StaticString = #file, line: UInt = #line) {
+        size.assert(count, message: message, file: file, line: line)
+    }
+}
